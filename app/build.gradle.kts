825c4caaf17e90b5b5ee2037f9ffd1456a400440
Doohyun <skaengus2012@naver.com>
2021-05-30 19:25:46 +0900

    Implement #1 - Add Test library.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 The N's lab Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 plugins {
     id("com.android.application")
     kotlin("android")
@@ -6,6 +23,7 @@ plugins {
 android {
     compileSdkVersion(AndroidConfig.COMPILE_SDK_VERSION)
     buildToolsVersion(AndroidConfig.BUILD_TOOLS_VERSION)
+
     defaultConfig {
         applicationId = "com.nlab.practice"
         minSdkVersion(AndroidConfig.MIN_SDK_VERSION)
@@ -22,23 +40,44 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
+    packagingOptions {
+        exclude("DebugProbesKt.bin")
+    }
+
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 }
 
 dependencies {
     implementation(Dependencies.KOTLIN)
+    implementation(Dependencies.KOTLIN_COROUTINE)
+    implementation(Dependencies.KOTLIN_COROUTINE_ANDROID)
+
     implementation(Dependencies.ANDROID_KTX)
     implementation(Dependencies.ANDROID_APPCOMPAT)
     implementation(Dependencies.ANDROID_MATERIAL)
     implementation(Dependencies.ANDROID_CONSTRAINT_LAYOUT)
+    implementation(Dependencies.ANDROID_RECYCLER_VIEW)
+    implementation(Dependencies.ANDROID_LIFECYCLE_VIEWMODEL)
+
     testImplementation(Dependencies.TEST_JUNIT)
+    testImplementation(Dependencies.TEST_MOCKITO)
     androidTestImplementation(Dependencies.TEST_ANDROID_JUNIT_EXT)
     androidTestImplementation(Dependencies.TEST_ANDROID_JUNIT_ESPRESSO)
+    androidTestImplementation(Dependencies.TEST_ANDROID_TEST_RUNNER)
+    androidTestImplementation(Dependencies.TEST_ANDROID_TEST_RULES)
+    androidTestImplementation(Dependencies.TEST_MOCKITO)
+    androidTestImplementation(Dependencies.TEST_DEX_MAKER)
+    androidTestImplementation(Dependencies.TEST_ROBOLECTRIC)
 }
