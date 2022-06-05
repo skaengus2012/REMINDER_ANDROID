df54785bce71b0dfff077d88e84ee476d9746cc3
skaengus2012 <skaengus2012@gmail.com>
2022-06-05 15:36:11 +0900

    Fix #9 - target, compile sdk version update

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -19,10 +19,10 @@
  * @author Doohyun
  */
 object AndroidConfig {
-    const val COMPILE_SDK_VERSION = 30
+    const val COMPILE_SDK_VERSION = 32
     const val BUILD_TOOLS_VERSION = "30.0.3"
     const val MIN_SDK_VERSION = 21
-    const val TARGET_SDK_VERSION = 30
+    const val TARGET_SDK_VERSION = 32
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
 }
