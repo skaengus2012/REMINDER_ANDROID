427fb393dde50fc293bdc68aed38722762e6760a
Doohyun Nam <skaengus2012@naver.com>
2022-06-30 00:38:37 +0900

    Fix #18 - add TestViewModel.kt for check ci

---
+++ app/src/main/java/com/nlab/reminder/domain/feature/home/TestViewModel.kt
@@ -0,0 +1,12 @@
+package com.nlab.reminder.domain.feature.home
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author Doohyun
+ */
+class TestViewModel : ViewModel() {
+    fun hello() {
+        println("Hello ci")
+    }
+}
