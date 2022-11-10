cb0522b349f08648bcf0e9bf20009b0681431d0e
Doohyun Nam <skaengus2012@naver.com>
2022-11-10 20:23:10 +0900

    Fix N/A - update library version to latest

--- buildSrc/src/main/kotlin/DependenciesVersions.kt
+++ buildSrc/src/main/kotlin/DependenciesVersions.kt
@@ -23,22 +23,22 @@ object DependenciesVersions {
     val JAVA_VERSION = JavaVersion.VERSION_11
     const val AGP = "7.3.1"
     const val JACOCO = "0.8.8"
-    const val KOTLIN = "1.7.10"
+    const val KOTLIN = "1.7.21"
     const val KOTLIN_COROUTINE = "1.6.4"
 
-    const val ANDROID_APPCOMPAT = "1.5.0"
-    const val ANDROID_CORE = "1.8.0"
+    const val ANDROID_APPCOMPAT = "1.5.1"
+    const val ANDROID_CORE = "1.9.0"
     const val ANDROID_MATERIAL = "1.6.1"
     const val ANDROID_CONSTRAINTLAYOUT = "2.1.4"
     const val ANDROID_RECYCLERVIEW = "1.2.1"
     const val ANDROID_LIFECYCLE = "2.5.1"
-    const val ANDROID_FRAGMENT = "1.5.2"
-    const val ANDROID_NAVIGATION = "2.5.1"
+    const val ANDROID_FRAGMENT = "1.5.4"
+    const val ANDROID_NAVIGATION = "2.5.3"
     const val ANDROID_ROOM = "2.4.3"
     const val ANDROID_STARTUP = "1.1.1"
     const val ANDROID_PAGING = "3.1.1"
 
-    const val GOOGLE_HILT = "2.43.2"
+    const val GOOGLE_HILT = "2.44"
     const val GOOGLE_FLEXBOX = "3.0.0"
 
     const val FACEBOOK_FLIPPER = "0.173.0"
@@ -48,7 +48,7 @@ object DependenciesVersions {
 
     const val TEST_JUNIT = "4.13.2"
     const val TEST_COROUTINES = "1.6.4"
-    const val TEST_MOCKITO = "4.7.0"
+    const val TEST_MOCKITO = "4.8.1"
     const val TEST_MOCKITO_KOTLIN = "4.0.0"
     const val TEST_JAVAFAKER = "1.0.2"
     const val TEST_ANDROID_JUNIT_EXT = "1.1.3"
