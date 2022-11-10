7fe31f0d9afe6cd6ab96304c40544565053c3005
Doohyun Nam <skaengus2012@naver.com>
2022-11-10 20:22:35 +0900

    Fix N/A - update target sdk

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -18,10 +18,10 @@
  * @author Doohyun
  */
 object AndroidConfig {
-    const val COMPILE_SDK_VERSION = 32
+    const val COMPILE_SDK_VERSION = 33
     const val BUILD_TOOLS_VERSION = "30.0.3"
     const val MIN_SDK_VERSION = 23
-    const val TARGET_SDK_VERSION = 32
+    const val TARGET_SDK_VERSION = 33
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
 }
