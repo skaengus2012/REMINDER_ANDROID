59ded2373fb4a0b3487c55723b270703f20078b5
Doohyun <skaengus2012@gmail.com>
2023-03-16 11:02:50 +0900

    Implement #88 - Add `DefaultUpdateSourceHandle`

---
+++ app/src/test/java/com/nlab/reminder/core/state2/middleware/handle/DefaultUpdateSourceHandleTest.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2023 The N's lab Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nlab.reminder.core.state2.middleware.handle
+
+import com.nlab.reminder.core.state2.TestAction
+import com.nlab.reminder.core.state2.TestState
+import com.nlab.reminder.core.state2.UpdateSource
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+/**
+ * @author thalys
+ */
+@ExperimentalCoroutinesApi
+class DefaultUpdateSourceHandleTest {
+    @Test
+    fun test() = runTest {
+        val inputSource = UpdateSource(TestAction.genAction(), TestState.genState())
+        val actionDispatcher: SuspendActionDispatcher<TestAction> = mock()
+        val handle = DefaultUpdateSourceHandle<TestAction, TestState> { source -> dispatch(source.action) }
+        handle.invoke(actionDispatcher, inputSource)
+
+        verify(actionDispatcher).dispatch(inputSource.action)
+    }
+
+}
