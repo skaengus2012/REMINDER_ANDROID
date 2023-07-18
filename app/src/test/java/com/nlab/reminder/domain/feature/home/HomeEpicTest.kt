2e35bb8c04dcecfbe71d85fa40f5a0221f0727e5
Doohyun <skaengus2012@gmail.com>
2023-07-19 00:44:11 +0900

    Implement #183 - Add `EpicTester`

--- app/src/test/java/com/nlab/reminder/domain/feature/home/HomeEpicTest.kt
+++ app/src/test/java/com/nlab/reminder/domain/feature/home/HomeEpicTest.kt
@@ -16,23 +16,11 @@
 
 package com.nlab.reminder.domain.feature.home
 
-import com.nlab.reminder.core.statekit.checkWhileStateUsed
-import com.nlab.reminder.domain.common.data.model.Tag
 import com.nlab.reminder.domain.common.data.model.genTags
-import com.nlab.statekit.util.buildDslInterceptor
-import com.nlab.statekit.util.createStore
+import com.nlab.statekit.test.tester
 import com.nlab.testkit.genInt
 import com.nlab.testkit.genLong
-import com.nlab.testkit.unconfinedCoroutineScope
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.runTest
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
@@ -43,66 +31,21 @@ import org.mockito.kotlin.whenever
  */
 internal class HomeEpicTest {
     @Test
-    fun `Repository used while subscribed`() = runTest {
-        val todaySchedulesCount = MutableStateFlow(0L)
-        val timetableSchedulesCount = MutableStateFlow(0L)
-        val allSchedulesCount = MutableStateFlow(0L)
-        val tags = MutableStateFlow<List<Tag>>(emptyList())
-        val store = createStore(
-            unconfinedCoroutineScope(),
-            HomeUiState.Loading,
-            epic = HomeEpic(
-                tagRepository = mock {
-                    whenever(mock.get()) doReturn tags
-                },
-                scheduleRepository = mock {
-                    whenever(mock.getTodaySchedulesCount()) doReturn todaySchedulesCount
-                    whenever(mock.getTimetableSchedulesCount()) doReturn timetableSchedulesCount
-                    whenever(mock.getAllSchedulesCount()) doReturn allSchedulesCount
-                }
-            )
-        )
-        store.checkWhileStateUsed(
-            todaySchedulesCount,
-            timetableSchedulesCount,
-            allSchedulesCount,
-            tags,
-        )
-    }
-
-    @Test
-    fun `Summary Loaded, when dependencies data changed`() = runTest {
+    fun `Loaded summary from repository`() {
         val todaySchedulesCount = genLong()
         val timetableSchedulesCount = genLong()
         val allSchedulesCount = genLong()
         val tags = genTags(count = genInt(min = 1, max = 3))
 
-        val awaitSummaryLoadedReceived = CompletableDeferred<HomeAction.SummaryLoaded>()
-        val store = createStore(
-            unconfinedCoroutineScope(),
-            HomeUiState.Loading,
-            interceptor = buildDslInterceptor {
-                action {
-                    anyState { awaitSummaryLoadedReceived.complete(it.action) }
-                }
-            },
-            epic = HomeEpic(
-                tagRepository = mock {
-                    whenever(mock.get()) doReturn flowOf(tags)
-                },
-                scheduleRepository = mock {
-                    whenever(mock.getTodaySchedulesCount()) doReturn flowOf(todaySchedulesCount)
-                    whenever(mock.getTimetableSchedulesCount()) doReturn flowOf(timetableSchedulesCount)
-                    whenever(mock.getAllSchedulesCount()) doReturn flowOf(allSchedulesCount)
-                }
-            )
-        )
-        val collectJob = launch { store.state.collect() }
-        val result = awaitSummaryLoadedReceived.await()
-
-        assertThat(
-            result,
-            equalTo(
+        HomeEpic(
+            tagRepository = mock { whenever(mock.get()) doReturn flowOf(tags) },
+            scheduleRepository = mock {
+                whenever(mock.getTodaySchedulesCount()) doReturn flowOf(todaySchedulesCount)
+                whenever(mock.getTimetableSchedulesCount()) doReturn flowOf(timetableSchedulesCount)
+                whenever(mock.getAllSchedulesCount()) doReturn flowOf(allSchedulesCount)
+            })
+            .tester()
+            .expectedAction(
                 HomeAction.SummaryLoaded(
                     todaySchedulesCount = todaySchedulesCount,
                     timetableSchedulesCount = timetableSchedulesCount,
@@ -110,7 +53,6 @@ internal class HomeEpicTest {
                     tags = tags
                 )
             )
-        )
-        collectJob.cancelAndJoin()
+            .verify()
     }
 }
