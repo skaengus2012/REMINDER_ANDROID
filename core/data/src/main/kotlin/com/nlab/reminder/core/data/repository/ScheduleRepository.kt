68077c15250067e96fb9bab859983b0f27e5577a
Doohyun Nam <skaengus2012@naver.com>
2024-07-05 21:21:00 +0900

    Fix #293 - attach generated annotation

--- core/data/src/main/kotlin/com/nlab/reminder/core/data/repository/ScheduleRepository.kt
+++ core/data/src/main/kotlin/com/nlab/reminder/core/data/repository/ScheduleRepository.kt
@@ -16,6 +16,7 @@
 
 package com.nlab.reminder.core.data.repository
 
+import com.nlab.reminder.core.annotation.test.ExcludeGeneratedFromTestReport
 import com.nlab.reminder.core.kotlin.Result
 import com.nlab.reminder.core.data.model.Schedule
 import com.nlab.reminder.core.data.model.ScheduleId
@@ -34,17 +35,28 @@ interface ScheduleRepository {
 }
 
 sealed class ScheduleGetStreamRequest private constructor() {
+    @ExcludeGeneratedFromTestReport
     data object All : ScheduleGetStreamRequest()
+
+    @ExcludeGeneratedFromTestReport
     data class ByComplete(val isComplete: Boolean) : ScheduleGetStreamRequest()
 }
 
 sealed class ScheduleUpdateRequest private constructor() {
+    @ExcludeGeneratedFromTestReport
     data class Completes(val idToCompleteTable: Map<ScheduleId, Boolean>) : ScheduleUpdateRequest()
+
+    @ExcludeGeneratedFromTestReport
     data class VisiblePriority(val idToVisiblePriorityTable: Map<ScheduleId, Long>) : ScheduleUpdateRequest()
 }
 
 sealed class ScheduleDeleteRequest private constructor() {
+    @ExcludeGeneratedFromTestReport
     data class ByComplete(val isComplete: Boolean) : ScheduleDeleteRequest()
+
+    @ExcludeGeneratedFromTestReport
     data class ById(val scheduleId: ScheduleId) : ScheduleDeleteRequest()
+
+    @ExcludeGeneratedFromTestReport
     data class ByIds(val scheduleIds: Collection<ScheduleId>) : ScheduleDeleteRequest()
 }
