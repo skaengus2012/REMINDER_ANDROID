0d42b04be0337be044213cdeeebc2342a9ad7a3b
Doohyun Nam <skaengus2012@naver.com>
2025-06-08 09:36:13 +0900

    Refactor #471 - migration joinToUiText ans nested uiText

--- core/data-format/src/main/kotlin/com/nlab/reminder/core/data/model/ui/ContentEveryRepeatFormats.kt
+++ core/data-format/src/main/kotlin/com/nlab/reminder/core/data/model/ui/ContentEveryRepeatFormats.kt
@@ -25,6 +25,7 @@ import com.nlab.reminder.core.kotlin.PositiveInt
 import com.nlab.reminder.core.kotlin.collections.NonEmptySet
 import com.nlab.reminder.core.text.PluralsUiText
 import com.nlab.reminder.core.text.UiText
+import com.nlab.reminder.core.text.joinToUiText
 import com.nlab.reminder.core.translation.PluralsIds
 import com.nlab.reminder.core.translation.StringIds
 import kotlinx.datetime.DayOfWeek
@@ -33,43 +34,32 @@ import kotlinx.datetime.Month
 /**
  * @author Doohyun
  */
-internal inline fun contentEveryHours(
-    interval: PositiveInt,
-    toText: UiText.() -> String
-): String {
-    val uiText = PluralsUiText(
+internal fun contentEveryHours(interval: PositiveInt): UiText =
+    PluralsUiText(
         resId = PluralsIds.content_every_hours_plurals,
         count = interval.value,
         interval.value
     )
-    return uiText.toText()
-}
 
-internal inline fun contentEveryDays(
-    interval: PositiveInt,
-    toText: UiText.() -> String
-): String {
-    val uiText = PluralsUiText(
+internal fun contentEveryDays(interval: PositiveInt): UiText =
+    PluralsUiText(
         resId = PluralsIds.content_every_days_plurals,
         count = interval.value,
         interval.value
     )
-    return uiText.toText()
-}
 
-internal inline fun contentEveryWeeks(
+internal fun contentEveryWeeks(
     interval: PositiveInt,
     dayOfWeeks: NonEmptySet<DayOfWeek>,
-    isSameDayOfWeek: Boolean,
-    crossinline toText: UiText.() -> String
-): String {
+    isSameDayOfWeek: Boolean
+): UiText {
     val contentEveryPlurals = PluralsUiText(
         resId = PluralsIds.content_every_weeks_plurals,
         count = interval.value,
         interval.value
     )
     if (isSameDayOfWeek) {
-        return contentEveryPlurals.toText()
+        return contentEveryPlurals
     }
     val dayOfWeeksText = if (dayOfWeeks.value.size == 1) {
         contentDayOfWeekUiText(dayOfWeeks.value.first())
@@ -79,27 +69,23 @@ internal inline fun contentEveryWeeks(
             resId = StringIds.content_every_weeks_token,
             sortedDayOfWeeks
                 .take(sortedDayOfWeeks.size - 1)
-                .joinToString(separator = ", ", transform = { contentDayOfWeekUiText(it).toText() }),
+                .joinToUiText(separatorRes = StringIds.comma_combine, transform = ::contentDayOfWeekUiText),
             contentDayOfWeekUiText(sortedDayOfWeeks.last())
         )
     }
-    val uiText = UiText(
+    return UiText(
         resId = StringIds.content_every_weeks_combine,
-        contentEveryPlurals.toText(),
-        dayOfWeeksText.toText()
+        contentEveryPlurals,
+        dayOfWeeksText
     )
-    return uiText.toText()
 }
 
-internal inline fun contentEveryMonthsWithEachOption(
+internal fun contentEveryMonthsWithEachOption(
     interval: PositiveInt,
     option: MonthlyRepeatDetail.Each,
     isSameDay: Boolean,
-    crossinline toText: UiText.() -> String
-): String {
-    if (isSameDay) {
-        return UiText(resId = StringIds.content_every_months).toText()
-    }
+): UiText {
+    if (isSameDay) return UiText(resId = StringIds.content_every_months)
 
     val contentEveryPlurals = PluralsUiText(
         resId = PluralsIds.content_every_months_plurals,
@@ -114,50 +100,40 @@ internal inline fun contentEveryMonthsWithEachOption(
             resId = StringIds.content_every_months_days_token,
             sortedDays
                 .take(sortedDays.size - 1)
-                .joinToString(separator = ", ", transform = { contentEveryMonthsDaysUiText(it).toText() }),
+                .joinToUiText(separatorRes = StringIds.comma_combine, transform = ::contentEveryMonthsDaysUiText),
             contentEveryMonthsDaysUiText(sortedDays.last())
         )
     }
-    val uiText = UiText(
+    return UiText(
         resId = StringIds.content_every_months_days_combine,
-        contentEveryPlurals.toText(),
-        daysText.toText()
+        contentEveryPlurals,
+        daysText
     )
-    return uiText.toText()
 }
 
-internal inline fun contentEveryMonthsWithCustomizeOption(
+internal fun contentEveryMonthsWithCustomizeOption(
     interval: PositiveInt,
-    option: MonthlyRepeatDetail.Customize,
-    crossinline toText: UiText.() -> String
-): String {
-    val everyContent = PluralsUiText(
+    option: MonthlyRepeatDetail.Customize
+): UiText = UiText(
+    resId = StringIds.content_every_months_customize_combine,
+    PluralsUiText(
         resId = PluralsIds.content_every_months_plurals,
         count = interval.value,
         interval.value
-    )
-    val orderingDaysText = contentOrderingDaysCombineUiText(
+    ),
+    contentOrderingDaysCombineUiText(
         order = option.order,
         day = option.day
     )
-    val uiText = UiText(
-        resId = StringIds.content_every_months_customize_combine,
-        everyContent.toText(),
-        orderingDaysText.toText()
-    )
-    return uiText.toText()
-}
+)
 
-internal inline fun convertEveryYears(
+internal fun convertEveryYears(
     interval: PositiveInt,
     months: NonEmptySet<Month>,
     daysOfWeekOption: YearlyDaysOfWeekOption?,
     isSameMonth: Boolean,
-    crossinline toText: UiText.() -> String
-): String {
-    if (isSameMonth && daysOfWeekOption == null) {
-        return UiText(resId = StringIds.content_every_years).toText()
-    }
+): UiText {
+    if (isSameMonth && daysOfWeekOption == null) return UiText(resId = StringIds.content_every_years)
 
     val contentEveryPlurals = PluralsUiText(
         resId = PluralsIds.content_every_years_plurals,
@@ -171,29 +147,28 @@ internal inline fun convertEveryYears(
             resId = StringIds.content_every_years_months_token,
             sortedMonths
                 .take(sortedMonths.size - 1)
-                .joinToString(separator = ", ", transform = { contentMonthUiText(it).toText() }),
+                .joinToUiText(separatorRes = StringIds.comma_combine, transform = ::contentMonthUiText),
             contentMonthUiText(sortedMonths.last())
         )
     }
-    val uiText = if (daysOfWeekOption == null) {
+
+    return if (daysOfWeekOption == null) {
         UiText(
             resId = StringIds.content_every_years_months_combine,
-            contentEveryPlurals.toText(),
-            monthsText.toText()
+            contentEveryPlurals,
+            monthsText
         )
     } else {
-        val orderingDaysCombineUiText = contentOrderingDaysCombineUiText(
-            order = daysOfWeekOption.order,
-            day = daysOfWeekOption.day
-        )
         UiText(
             resId = StringIds.content_every_years_ordering_days_months_combine,
-            contentEveryPlurals.toText(),
-            orderingDaysCombineUiText.toText(),
-            monthsText.toText()
+            contentEveryPlurals,
+            contentOrderingDaysCombineUiText(
+                order = daysOfWeekOption.order,
+                day = daysOfWeekOption.day
+            ),
+            monthsText
         )
     }
-    return uiText.toText()
 }
 
 private fun contentDayOfWeekUiText(dayOfWeek: DayOfWeek): UiText =
