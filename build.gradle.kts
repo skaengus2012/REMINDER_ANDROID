44af15dabb5956417abc65461a17fa1722298203
Doohyun Nam <skaengus2012@gmail.com>
2025-07-24 21:00:35 +0900

    Implement #527- add kover dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,5 +20,6 @@ plugins {
     alias(libs.plugins.google.hilt) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlinx.kover) apply false
     alias(libs.plugins.ksp) apply false
 }
