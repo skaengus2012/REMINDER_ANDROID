31a79b5e952b63be9322cc7406ccd7f5c713b996
Doohyun Nam <skaengus2012@gmail.com>
2025-07-30 06:21:06 +0900

    Fix #520 - restore `StateScopeReduceBuilderTest`

--- statekit/dsl/src/test/kotlin/com/nlab/statekit/dsl/reduce/StateScopeReduceBuilderTest.kt
+++ statekit/dsl/src/test/kotlin/com/nlab/statekit/dsl/reduce/StateScopeReduceBuilderTest.kt
@@ -18,172 +18,163 @@ package com.nlab.statekit.dsl.reduce
 
 import com.nlab.statekit.dsl.TestAction
 import com.nlab.statekit.dsl.TestState
-import com.nlab.statekit.dispatch.ActionDispatcher
+import com.nlab.statekit.test.reduce.expectedNextState
+import com.nlab.statekit.test.reduce.expectedNotChanged
+import io.mockk.coVerify
+import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
-import org.mockito.Mockito.mock
-import org.mockito.kotlin.never
-import org.mockito.kotlin.once
-import org.mockito.kotlin.verify
 
 /**
  * @author Doohyun
  */
 class StateScopeReduceBuilderTest {
-    /**
     @Test
-    fun `Given matched inputs and expected state, When transition with action type from builder, Then return expectedState`() {
+    fun `Given action1, When register transition that changes with action1, Then return correct state`() = runTest {
         val inputAction = TestAction.Action1
         val inputState = TestState.State1
         val expectedState = TestState.State2
         val reduceBuilder = TestStateScopeReduceBuilder {
-            transition<TestAction.Action1> {
-                if (current == inputState) expectedState
-                else current
-            }
+            transition<TestAction.Action1> { expectedState }
         }
         val transition = checkNotNull(reduceBuilder.delegate.buildTransition())
-        transition.assert(inputAction, inputState, expectedState)
+        transition.toReduceTestBuilder()
+            .givenCurrent(inputState)
+            .actionToDispatch(inputAction)
+            .transitionScenario()
+            .expectedNextState(expectedState)
     }
 
     @Test
-    fun `Given none matched inputs, When transition with action type form builder, Then return inputState`() {
-        val inputAction = TestAction.Action1
-        val inputState = TestState.State1
+    fun `Given action2, When register transition that changes with action1, Then state never changed`() = runTest {
+        val inputAction = TestAction.Action2
         val reduceBuilder = TestStateScopeReduceBuilder {
-            transition<TestAction.Action2> { TestState.State2 }
+            transition<TestAction.Action1> { error("Should not be called") }
         }
         val transition = checkNotNull(reduceBuilder.delegate.buildTransition())
-        transition.assert(inputAction, inputState, inputState)
+        transition.toReduceTestBuilder()
+            .givenCurrent(TestState.genState())
+            .actionToDispatch(inputAction)
+            .transitionScenario()
+            .expectedNotChanged()
     }
 
     @Test
-    fun `Given matched input and runner, When launch effect form builder, Then runner invoked`() = runTest {
+    fun `Given action1, When register effect on action1, Then runner invoked`() = runTest {
         val inputAction = TestAction.Action1
-        val inputState = TestState.State1
-        val runner: () -> Unit = mock()
+        val runner: () -> Unit = mockk(relaxed = true)
         val reduceBuilder = TestStateScopeReduceBuilder {
-            effect {
-                if (action == inputAction && current == inputState) {
-                    runner.invoke()
-                }
+            effect<TestAction.Action1> { runner.invoke() }
+        }
+        val effect = checkNotNull(reduceBuilder.delegate.buildEffect())
+        effect.toReduceTestBuilder()
+            .givenCurrent(TestState.genState())
+            .actionToDispatch(inputAction)
+            .effectScenario()
+            .launchAndGetTrace()
+        verify(exactly = 1) {
+            runner.invoke()
+        }
+    }
+
+    @Test
+    fun `Given action2, When register effect on action1, Then runner never invoked`() = runTest {
+        val inputAction = TestAction.Action2
+        val runner: () -> Unit = mockk()
+        val reduceBuilder = TestStateScopeReduceBuilder {
+            effect<TestAction.Action1> { runner.invoke() }
+        }
+        val effect = checkNotNull(reduceBuilder.delegate.buildEffect())
+        effect.toReduceTestBuilder()
+            .givenCurrent(TestState.genState())
+            .actionToDispatch(inputAction)
+            .effectScenario()
+            .launchAndGetTrace()
+        verify(inverse = true) {
+            runner.invoke()
+        }
+    }
+
+    @Test
+    fun `Given action1, When register suspend effect on action1, Then runner invoked`() = runTest {
+        val inputAction = TestAction.Action1
+        val runner: suspend () -> Unit = mockk(relaxed = true)
+        val reduceBuilder = TestStateScopeReduceBuilder {
+            suspendEffect<TestAction.Action1> { runner.invoke() }
+        }
+        val effect = checkNotNull(reduceBuilder.delegate.buildEffect())
+        effect.toReduceTestBuilder()
+            .givenCurrent(TestState.genState())
+            .actionToDispatch(inputAction)
+            .effectScenario()
+            .launchAndGetTrace()
+        coVerify(exactly = 1) {
+            runner.invoke()
+        }
+    }
+
+    @Test
+    fun `Given matched predicate, When register effect in scope with predicate, Then runner invoked`() = runTest {
+        val predicate: (TestUpdateSource) -> Boolean = { true }
+        val runner: () -> Unit = mockk(relaxed = true)
+        val reduceBuilder = TestStateScopeReduceBuilder {
+            scope(isMatch = predicate) {
+                effect { runner.invoke() }
             }
         }
         val effect = checkNotNull(reduceBuilder.delegate.buildEffect())
-        effect.launchAndJoinForTest(inputAction, inputState)
-        verify(runner, once()).invoke()
-    }
-
-    @Test
-    fun `Given input state and runner, When launch effect with action type form builder, Then runner invoked conditionally`() = runTest {
-        suspend fun invokeWhenStateTypeIsAction1(
-            inputAction: TestAction,
-            runner: () -> Unit
-        ) {
-            val reduceBuilder = TestStateScopeReduceBuilder {
-                effect<TestAction.Action1> { runner.invoke() }
-            }
-            val effect = checkNotNull(reduceBuilder.delegate.buildEffect())
-            effect.launchAndJoinForTest(inputAction, TestState.genState())
+        effect.toReduceTestBuilder()
+            .givenCurrent(TestState.genState())
+            .actionToDispatch(TestAction.genAction())
+            .effectScenario()
+            .launchAndGetTrace()
+        verify(exactly = 1) {
+            runner.invoke()
         }
-
-        val runnerWithAction1: () -> Unit = mock()
-        val runnerWithAction2: () -> Unit = mock()
-        invokeWhenStateTypeIsAction1(inputAction = TestAction.Action1, runnerWithAction1)
-        invokeWhenStateTypeIsAction1(inputAction = TestAction.Action2, runnerWithAction2)
-
-        verify(runnerWithAction1, once()).invoke()
-        verify(runnerWithAction2, never()).invoke()
     }
 
     @Test
-    fun `Given matched input and actionDispatcher, When launch suspend effect from builder, Then actionDispatcher invoked`() = runTest {
-        val inputAction = TestAction.Action1
-        val inputState = TestState.State1
-        val expectedAction = TestAction.Action2
-        val actionDispatcher: ActionDispatcher<TestAction> = mock()
+    fun `Given transformSource, When register effect in scope with transformSource, Then runner invoked`() = runTest {
+        val transformSource: (TestUpdateSource) -> UpdateSource<Int, TestState>? = {
+            UpdateSource(action = 1, current = it.current)
+        }
+        val runner: () -> Unit = mockk(relaxed = true)
         val reduceBuilder = TestStateScopeReduceBuilder {
-            suspendEffect {
-                if (action == inputAction && current == inputState) dispatch(expectedAction)
+            scope(transformSource = transformSource) {
+                effect { runner.invoke() }
             }
         }
         val effect = checkNotNull(reduceBuilder.delegate.buildEffect())
-        effect.launchAndJoinForTest(inputAction, inputState, actionDispatcher)
-        verify(actionDispatcher, once()).dispatch(expectedAction)
-    }
-
-    @Test
-    fun `Given matched inputs and actionDispatcher, When launch suspend effect with action type from builder, Then actionDispatcher invoked`() = runTest {
-        val inputAction = TestAction.Action1
-        val inputState = TestState.State1
-        val expectedAction = TestAction.Action2
-        val actionDispatcher: ActionDispatcher<TestAction> = mock()
-        val reduceBuilder = TestStateScopeReduceBuilder {
-            suspendEffect<TestAction.Action1> {
-                if (current == inputState) dispatch(expectedAction)
-            }
+        effect.toReduceTestBuilder()
+            .givenCurrent(TestState.genState())
+            .actionToDispatch(TestAction.genAction())
+            .effectScenario()
+            .launchAndGetTrace()
+        verify(exactly = 1) {
+            runner.invoke()
         }
-        val effect = checkNotNull(reduceBuilder.delegate.buildEffect())
-        effect.launchAndJoinForTest(inputAction, inputState, actionDispatcher)
-        verify(actionDispatcher, once()).dispatch(expectedAction)
     }
 
     @Test
-    fun `Given inputs and matched predicated block, When transition with predicate scope from builder, Then return expectedState`() {
-        val inputAction = TestAction.Action1
-        val inputState = TestState.State1
-        val expectedState = TestState.State2
-        val reduceBuilder = TestStateScopeReduceBuilder {
-            scope(isMatch = { action == inputAction && current == inputState }) {
-                transition { expectedState }
-            }
-        }
-        val transition = checkNotNull(reduceBuilder.delegate.buildTransition())
-        transition.assert(inputAction, inputState, expectedState)
-    }
-
-    @Test
-    fun `Given inputs, actionDispatcher and transformSource block, When launch suspend effect with transformSource scope from builder, Then actionDispatcher invoked`() = runTest {
-        val inputAction = TestAction.Action1
-        val inputState = TestState.State1
-        val expectedAction = TestAction.Action2
-        val actionDispatcher: ActionDispatcher<TestAction> = mock()
-        val reduceBuilder = TestStateScopeReduceBuilder {
-            scope(transformSource = {
-                if (action == inputAction && current == inputState) UpdateSource(action = 1, current = current)
-                else null
-            }) {
-                scope(transformSource = { UpdateSource(action = action + 2, current = current) }) {
-                    suspendEffect {
-                        if (action == 3) {
-                            dispatch(expectedAction)
-                        }
-                    }
-                }
-            }
-        }
-        val effect = checkNotNull(reduceBuilder.delegate.buildEffect())
-        effect.launchAndJoinForTest(inputAction, inputState, actionDispatcher)
-        verify(actionDispatcher, once()).dispatch(expectedAction)
-    }
-
-    @Test
-    fun `Given inputs, actionType and actionDispatcher, When launch suspend effect with stateType scope from builder, Then actionDispatcher invoked`() = runTest {
-        val inputAction = TestAction.Action1
-        val inputState = TestState.State1
-        val expectedAction = TestAction.Action2
-        val actionDispatcher: ActionDispatcher<TestAction> = mock()
+    fun `Given state1 input, When register effect in scope with state1 filter, Then runner invoked`() = runTest {
+        val input = TestState.State1
+        val runner: () -> Unit = mockk(relaxed = true)
         val reduceBuilder = TestStateScopeReduceBuilder {
             scope<TestState.State1> {
-                suspendEffect {
-                    if (action == inputAction) dispatch(expectedAction)
-                }
+                effect { runner.invoke() }
             }
         }
         val effect = checkNotNull(reduceBuilder.delegate.buildEffect())
-        effect.launchAndJoinForTest(inputAction, inputState, actionDispatcher)
-        verify(actionDispatcher, once()).dispatch(expectedAction)
-    }*/
+        effect.toReduceTestBuilder()
+            .givenCurrent(input)
+            .actionToDispatch(TestAction.genAction())
+            .effectScenario()
+            .launchAndGetTrace()
+        verify(exactly = 1) {
+            runner.invoke()
+        }
+    }
 }
 
 @Suppress("TestFunctionName")
