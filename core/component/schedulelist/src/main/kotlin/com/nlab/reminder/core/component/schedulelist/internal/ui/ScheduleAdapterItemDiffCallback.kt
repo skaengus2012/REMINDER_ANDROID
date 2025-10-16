d697fcacfbe3f5c37a12a2509890d7ca6b77334d
Doohyun Nam <skaengus2012@gmail.com>
2025-10-16 21:56:53 +0900

    Refactor #585 - Transfer packages from 'ScheduleList' item items to the internal area

---
+++ core/component/schedulelist/src/main/kotlin/com/nlab/reminder/core/component/schedulelist/internal/ui/ScheduleAdapterItemDiffCallback.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2025 The N's lab Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nlab.reminder.core.component.schedulelist.internal.ui
+
+import androidx.recyclerview.widget.DiffUtil
+
+/**
+ * @author Thalys
+ */
+internal class ScheduleAdapterItemDiffCallback : DiffUtil.ItemCallback<ScheduleAdapterItem>() {
+    override fun areItemsTheSame(oldItem: ScheduleAdapterItem, newItem: ScheduleAdapterItem): Boolean = when {
+        oldItem is ScheduleAdapterItem.Content && newItem is ScheduleAdapterItem.Content -> {
+            oldItem.schedule.resource.id == newItem.schedule.resource.id
+        }
+
+        else -> oldItem === newItem
+    }
+
+    override fun areContentsTheSame(oldItem: ScheduleAdapterItem, newItem: ScheduleAdapterItem): Boolean =
+        oldItem == newItem
+}
---
+++ core/component/schedulelist/src/main/kotlin/com/nlab/reminder/core/component/schedulelist/internal/ui/ScheduleListSelectionSource.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2025 The N's lab Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nlab.reminder.core.component.schedulelist.internal.ui
+
+import com.nlab.reminder.core.data.model.ScheduleId
+
+/**
+ * @author Doohyun
+ */
+interface ScheduleListSelectionSource {
+    fun findScheduleId(position: Int): ScheduleId?
+    fun findSelected(scheduleId: ScheduleId): Boolean
+}
+
+fun ScheduleListSelectionSource(
+    adapter: ScheduleListAdapter
+): ScheduleListSelectionSource = object : ScheduleListSelectionSource {
+    override fun findScheduleId(position: Int): ScheduleId? {
+        return (adapter.getCurrentList().getOrNull(position) as? ScheduleAdapterItem.Content)
+            ?.schedule
+            ?.resource
+            ?.id
+    }
+
+    override fun findSelected(scheduleId: ScheduleId): Boolean {
+        return scheduleId in adapter.getCurrentSelected()
+    }
+}
