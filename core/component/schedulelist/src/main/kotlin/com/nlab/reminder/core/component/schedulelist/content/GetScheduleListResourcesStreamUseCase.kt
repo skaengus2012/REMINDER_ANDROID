710571aa2881695f646846adf4ef838bc3876a1a
Doohyun Nam <skaengus2012@gmail.com>
2025-10-30 20:47:51 +0900

    Implement #604 - add GetScheduleListResourcesStreamUseCase

---
+++ core/component/schedulelist/src/main/kotlin/com/nlab/reminder/core/component/schedulelist/content/GetScheduleListResourcesStreamUseCase.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2025 The N's lab Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nlab.reminder.core.component.schedulelist.content
+
+import com.nlab.reminder.core.data.model.Link
+import com.nlab.reminder.core.data.model.Schedule
+import com.nlab.reminder.core.data.model.TagId
+import com.nlab.reminder.core.data.repository.GetTagQuery
+import com.nlab.reminder.core.data.repository.LinkMetadataRepository
+import com.nlab.reminder.core.data.repository.TagRepository
+import com.nlab.reminder.core.kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+
+/**
+ * @author Thalys
+ */
+class GetScheduleListResourcesStreamUseCase(
+    private val tagRepository: TagRepository,
+    private val linkMetadataRepository: LinkMetadataRepository
+) {
+    operator fun invoke(
+        schedulesFlow: Flow<List<Schedule>>
+    ): Flow<List<ScheduleListResource>> = schedulesFlow.flatMapLatest { schedules ->
+        val tagIds = mutableSetOf<TagId>()
+        val links = mutableSetOf<Link>()
+        schedules.forEach { schedule ->
+            val content = schedule.content
+            content.link?.let(links::add)
+            content.tagIds.let(tagIds::addAll)
+        }
+        combine(
+            tagRepository.getTagsAsStream(GetTagQuery.ByIds(tagIds))
+                .map { tags -> tags.associateBy { it.id } },
+            linkMetadataRepository.getLinkToMetadataTableAsStream(links)
+        ) { tagIdToTagTable, linkToMetadataTable ->
+            fun transformToResource(schedule: Schedule): ScheduleListResource = ScheduleListResource(
+                id = schedule.id,
+                title = schedule.content.title,
+                note = schedule.content.note,
+                link = schedule.content.link,
+                linkMetadata = schedule.content.link?.let { linkToMetadataTable[it] },
+                timing = schedule.content.timing,
+                isComplete = schedule.isComplete,
+                tags = schedule.content.tagIds.asSequence()
+                    .mapNotNull { tagIdToTagTable[it] }
+                    .sortedBy { it.name.value }
+                    .toList()
+            )
+            schedules.map(::transformToResource)
+        }
+    }
+}
---
+++ core/component/schedulelist/src/test/kotlin/com/nlab/reminder/core/component/schedulelist/content/GetScheduleListResourcesStreamUseCaseTest.kt
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2025 The N's lab Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nlab.reminder.core.component.schedulelist.content
+
+import app.cash.turbine.test
+import com.nlab.reminder.core.data.model.genLinkMetadata
+import com.nlab.reminder.core.data.model.genSchedule
+import com.nlab.reminder.core.data.model.genScheduleContent
+import com.nlab.reminder.core.data.model.genSchedules
+import com.nlab.reminder.core.data.model.genTag
+import com.nlab.reminder.core.data.repository.GetTagQuery
+import com.nlab.reminder.core.data.repository.LinkMetadataRepository
+import com.nlab.reminder.core.data.repository.TagRepository
+import com.nlab.reminder.core.kotlin.collections.toSet
+import com.nlab.reminder.core.kotlin.toNonBlankString
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+/**
+ * @author Thalys
+ */
+class GetScheduleListResourcesStreamUseCaseTest {
+    @Test
+    fun `Given schedules, When collect, Then emit resources with identical content mapping`() = runTest {
+        val schedules = genSchedules()
+        val useCase = genGetScheduleListResourcesStreamUseCase()
+        useCase.invoke(flowOf(schedules)).test {
+            val actualResources = awaitItem()
+            actualResources.forEachIndexed { index, resource ->
+                val schedule = schedules[index]
+                assertThat(resource.id, equalTo(schedule.id))
+                assertThat(resource.title, equalTo(schedule.content.title))
+                assertThat(resource.note, equalTo(schedule.content.note))
+                assertThat(resource.link, equalTo(schedule.content.link))
+                assertThat(resource.timing, equalTo(schedule.content.timing))
+                assertThat(resource.isComplete, equalTo(schedule.isComplete))
+            }
+
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `Given schedules with tagIds, When collect, Then emit resources with matching sorted tags`() = runTest {
+        val schedules = genSchedules()
+        val tagIds = schedules.map { it.content.tagIds }
+            .flatten()
+            .distinct()
+            .toSet()
+        val tags = tagIds.toSet { tagId ->
+            genTag(id = tagId, name = tagId.rawId.toString().toNonBlankString())
+        }
+        val useCase = genGetScheduleListResourcesStreamUseCase(
+            tagRepository = mockk {
+                every { getTagsAsStream(GetTagQuery.ByIds(tagIds)) } returns flowOf(tags)
+            }
+        )
+        useCase.invoke(flowOf(schedules)).test {
+            val actualResources = awaitItem()
+            actualResources.forEachIndexed { index, resource ->
+                val expectedTags = schedules[index].content.tagIds
+                    .sortedBy { it.rawId }
+                    .map { tagId -> tags.find { it.id == tagId } }
+
+                assertThat(resource.tags, equalTo(expectedTags))
+            }
+
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `Given schedules with links, When collect, Then emit resources with matching link metadata`() = runTest {
+        val schedules = genSchedules()
+        val links = schedules.mapNotNull { it.content.link }.toSet()
+        val linkToMetadataTable = links.associateWith { genLinkMetadata() }
+        val useCase = genGetScheduleListResourcesStreamUseCase(
+            linkMetadataRepository = mockk {
+                every { getLinkToMetadataTableAsStream(links) } returns flowOf(linkToMetadataTable)
+            }
+        )
+        useCase.invoke(flowOf(schedules)).test {
+            val actualResources = awaitItem()
+            actualResources.forEachIndexed { index, resource ->
+                val expectedMetadata = schedules[index]
+                    .content
+                    .link
+                    ?.let { linkToMetadataTable[it] }
+                assertThat(resource.linkMetadata, equalTo(expectedMetadata))
+            }
+
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `Given schedule without link, When collect, Then emit resource with null link metadata`() = runTest {
+        val schedule = genSchedule(
+            content = genScheduleContent(link = null)
+        )
+        val useCase = genGetScheduleListResourcesStreamUseCase(
+            linkMetadataRepository = mockk {
+                every { getLinkToMetadataTableAsStream(any()) } returns flowOf(emptyMap())
+            }
+        )
+        useCase.invoke(flowOf(listOf(schedule))).test {
+            val actualResource = awaitItem().first()
+            assertThat(actualResource.linkMetadata, nullValue())
+
+            awaitComplete()
+        }
+    }
+}
+
+private fun genGetScheduleListResourcesStreamUseCase(
+    tagRepository: TagRepository = mockk {
+        every { getTagsAsStream(any()) } returns flowOf(emptySet())
+    },
+    linkMetadataRepository: LinkMetadataRepository = mockk {
+        every { getLinkToMetadataTableAsStream(any()) } returns flowOf(emptyMap())
+    }
+): GetScheduleListResourcesStreamUseCase = GetScheduleListResourcesStreamUseCase(
+    tagRepository = tagRepository,
+    linkMetadataRepository = linkMetadataRepository
+)
